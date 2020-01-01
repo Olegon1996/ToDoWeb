90608bf6790180136d3952d8d0fe93ec0128a8e2
Olegon1996 <kytsiko@gmail.com>
2020-01-01 14:40:18 +0200

    11

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,7 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import reducer from "./reducers/reducer";
 
-const store = createStore(
-  reducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store = createStore(reducer, applyMiddleware(thunk));
 
 export default store;
